0d53459536dfde8088eaaa2e2c0124d22e3c59f6
Sami <savasp@utu.fi>
2019-06-29 23:29:44 +0300

    Part 1.9 working

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,8 +2,14 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 
-const Statistics = (props) => {
-
+const Statistics = ({reviews}) => {
+  const {good, bad, neutral} = reviews;
+  return (
+    <div>
+      <p>avg {(good*1-bad*1)/(good+bad+neutral)}</p>
+      <p>positive {good/(bad+neutral)}%</p>
+    </div>
+  )
 }
 
 const App = () => {
@@ -12,17 +18,40 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  return (
-    <div>
+  const reviews = {
+    good: good,
+    neutral: neutral,
+    bad: bad
+  }
+  
+  
+  if(good !=0 || neutral != 0 || bad != 0 ){
+    return (
+      <div>
+      <h1>Give feedback</h1>
+      <button onClick={() => setGood(good +1)}>Good</button>
+      <button onClick={() => setNeutral(neutral +1)}>Neutral</button>
+      <button onClick={() => setBad(bad +1)}>Bad</button>
       <h1>Statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
       <p>all {good+bad+neutral}</p>
-      <p>avg </p>
-      <p>positive {good/(bad+neutral)}</p>
-    </div>
-  )
+      <Statistics reviews = {reviews}/>
+      </div>
+    )
+  } else { 
+    return (
+      <div>
+        <h1>Give feedback</h1>
+        <button onClick={() => setGood(good +1)}>Good</button>
+        <button onClick={() => setNeutral(neutral +1)}>Neutral</button>
+        <button onClick={() => setBad(bad +1)}>Bad</button>
+        <h1>Statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<App />, 
