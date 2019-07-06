67067fd4446c9fe47b17426dd6ed9ef96887467a
Sami <savasp@utu.fi>
2019-07-06 21:50:07 +0300

    Part 2.12. done

---
+++ part1/countries/src/Country.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Country = ({country}) => {
+    const {capital, population, languages, name, flag} = country;
+
+    const getLanguages = languages.map((lang) => {
+        return <ul>{lang.name}</ul>
+    })
+    return(
+        <div>
+            <h1>{name}</h1>
+            <li>Capital {capital}</li>
+            <li>Population {population}</li>
+            <h2>Languages</h2>
+            {getLanguages}
+            <br/>
+            <img width="250px" height="auto" border="1px solid black" src={flag} alt={name}/>
+        </div>
+    )
+}
+
+export default Country
