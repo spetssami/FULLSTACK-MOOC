26e7c61ce8c711bfabf6a055f42b53e3660ab961
Sami <savasp@utu.fi>
2019-08-04 00:22:35 +0300

    Removed unnecessary code

--- part1/puhelinluettelo/src/People.js
+++ part1/puhelinluettelo/src/People.js
@@ -6,16 +6,10 @@ const People = ({list, deletePerson}) => {
   const people = () => list.map((person) => {
     return <Person person = {person} deletePerson={() => deletePerson(person.name)} />
   })
-  //renderöi uuden henkilön
   return(
     <ul>
       {people()}
     </ul>
-    
-    // list.map((person) => {
-    //   return (<li key={person.id}>{person.name} - {person.number} <button onClick={() => {if(window.confirm(`Do you want to delete ${person.name} fromcontact info`)){
-    //     peopleService.deletePerson(person.id).then(alert(`${person.name} has been deleted`))}}}>Delete this</button></li>)
-    // })
   )
 
 }
